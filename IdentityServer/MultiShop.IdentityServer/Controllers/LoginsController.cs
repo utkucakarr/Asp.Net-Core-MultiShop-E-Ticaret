ae9937572a7e74725aaaf2c293c154467e1d6483
Utku Çakar <cakarutku1@hotmail.com>
2025-03-21 20:17:07 +0300

    Added token capture operations codes

--- IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.IdentityServer.Dtos;
 using MultiShop.IdentityServer.Models;
+using MultiShop.IdentityServer.Tools;
 using System.Threading.Tasks;
 
 namespace MultiShop.IdentityServer.Controllers
@@ -13,19 +14,26 @@ namespace MultiShop.IdentityServer.Controllers
     {
         //Burası giriş işlemi yapmak için 
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        public LoginsController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpPost]
         public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
         {
             var result = await _signInManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
+            var user = await _userManager.FindByNameAsync(userLoginDto.UserName);
             if (result.Succeeded)
             {
-                return Ok("Giriş başarılı");
+                GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+                model.UserName = userLoginDto.UserName;
+                model.Id = user.Id;
+                var token = JwtTokenGenerator.GenerateToken(model);
+                return Ok(token);
             }
             else
             {
---
+++ IdentityServer/MultiShop.IdentityServer/Tools/GetCheckAppUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.IdentityServer.Tools
+{
+    public class GetCheckAppUserViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Role { get; set; }
+
+        public string IsExist { get; set; }
+    }
+}
