4ba4ad12691b1e0a01884d02cff315fb61831f73
Utku Çakar <cakarutku1@hotmail.com>
2025-03-21 18:37:11 +0300

    Added login pages codes for API and WEBUI

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        //Burası giriş işlemi yapmak için 
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Ok("Giriş başarılı");
+            }
+            else
+            {
+                return Ok("Kullanıcı adı veya şifre hatalı");
+            }
+        }
+    }
+}
